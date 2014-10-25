2918b3bb81e851f1d448332959b5a74f6af774c7
Ryan Tablada <ryan.tablada@gmail.com>
2014-10-25 13:57:53 -0500

    Resource routes for news pages

--- app/router.js
+++ app/router.js
@@ -7,6 +7,12 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
 	this.route('music');
+
+	this.resource('news', function() {
+		this.route('show', {
+			path: '/:news_article_id'
+		});
+	});
 });
 
 export default Router;
