1980682c5b663630d851ba156ac9b0e5c6bf12c9
Ryan Tablada <ryan.tablada@gmail.com>
2014-11-02 21:50:39 -0600

    Slider animation sliding

--- app/components/slider-photo.js
+++ app/components/slider-photo.js
@@ -4,11 +4,12 @@ export default Ember.Component.extend({
 	tagName: 'img',
 	classNames: ['slider-photo'],
 	attributeBindings: ['photoUrl:src'],
-	classNameBindings: ['isSelected:selected'],
+	classNameBindings: ['isSelected:selected', 'isAfter:after', 'isBefore:before'],
 
 	photoUrl: null,
 	photos: [],
 	selectedIndex: null,
+	canSlideOut: true,
 
 	index: function() {
 		return this.get('photos').indexOf(this.get('photoUrl'));
@@ -16,5 +17,34 @@ export default Ember.Component.extend({
 
 	isSelected: function() {
 		return this.get('index') === this.get('selectedIndex');
-	}.property('index', 'selectedIndex')
+	}.property('index', 'selectedIndex'),
+
+	isBefore: function() {
+		return this.get('index') < this.get('selectedIndex');
+	}.property('index', 'selectedIndex'),
+
+	isAfter: function() {
+		return this.get('index') > this.get('selectedIndex');
+	}.property('index', 'selectedIndex'),
+
+	hasMovedBefore: function() {
+		if (this.get('isBefore') && this.get('canSlideOut')) {
+			this.$().velocity({left: '-1000px'});
+			this.set('canSlideOut', false);
+		}
+	}.observes('isBefore'),
+
+	hasMovedAfter: function() {
+		if (this.get('isAfter') && this.get('canSlideOut')) {
+			this.$().velocity({left: '1000px'});
+			this.set('canSlideOut', false);
+		}
+	}.observes('isAfter'),
+
+	hasMovedSelected: function() {
+		if (this.get('isSelected')) {
+			this.$().velocity({left: 0});
+			this.set('canSlideOut', true);
+		}
+	}.observes('isSelected')
 });
