fbeed0749fc2c8bebdbdd9dfddb7ea9881fa406f
Ryan Tablada <ryan.tablada@gmail.com>
2015-03-14 00:29:59 -0500

    Connect to Express server

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,3 +1,12 @@
 import DS from 'ember-data';
 
-export default DS.FixtureAdapter.extend();
+export default DS.RESTAdapter.extend({
+  namespace: 'api',
+
+  pathForType: function(type) {
+    var decamelized = Ember.String.decamelize(type);
+    decamelized = decamelized.replace('_', '-');
+
+    return Ember.String.pluralize(decamelized);
+  }
+});
