1cc13b2f01f0d1e4b5e3f266f49d703805651094
Ryan Tablada <ryan.tablada@gmail.com>
2015-05-04 21:30:18 -0500

    Styling Soundcloud Media Player

--- app/components/soundcloud-controller.js
+++ app/components/soundcloud-controller.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['soundcloud-controls'],
+
   volume: Ember.computed.alias('soundcloud.volume'),
 
   isPaused: Ember.computed.alias('soundcloud.paused'),
